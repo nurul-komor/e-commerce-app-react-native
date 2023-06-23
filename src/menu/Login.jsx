9724744ca5c0654b6e38a0b645ede9c10fdd3414
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-06-23 16:15:09 +0600

    login form updated

--- src/menu/Login.jsx
+++ src/menu/Login.jsx
@@ -1,18 +1,60 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import React from "react";
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import Checkbox from "expo-checkbox";
 export default function Login() {
+  const [agree, setAgree] = useState(false);
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Login</Text>
-      <Text style={styles.description}>Lorem ipsum dolor sit amet.</Text>
+      <Text style={styles.description}>please fill the field to continue</Text>
       <View>
         <Text style={styles.textLabel}>Enter you email</Text>
         <TextInput style={styles.textInput} autoCapitalize="none" />
       </View>
-      <View style={{ marginVertical: 10 }}>
+      <View style={styles.inputGroup}>
         <Text style={styles.textLabel}>Enter you Password</Text>
         <TextInput style={styles.textInput} secureTextEntry={true} />
+      </View>
+      <Text
+        style={[
+          styles.inputGroup,
+          {
+            display: "flex",
+            flexWrap: "wrap",
+          },
+        ]}
+      >
+        <Checkbox
+          value={agree}
+          onValueChange={() => setAgree(!agree)}
+          color={agree ? "#0984e3" : undefined}
+          style={{ marginRight: 5, display: "flex" }}
+        />
+        <Text
+          onPress={() => setAgree(!agree)}
+          style={{ padding: 5, marginBottom: 10, display: "flex" }}
+        >
+          {"  "}
+          Do you agree with terms and conditions?
+        </Text>
+      </Text>
+      <View style={styles.inputGroup}>
+        <TouchableOpacity disabled={agree ? false : true}>
+          <Text
+            style={[
+              styles.buttonStyle,
+              { backgroundColor: agree ? "#0984e3" : "#636e72" },
+            ]}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -25,7 +67,7 @@ const styles = StyleSheet.create({
     fontFamily: "poppins-regular",
     padding: 30,
     backgroundColor: "#fff",
-    height: "85%",
+    height: "95%",
   },
   heading: {
     fontSize: 30,
@@ -36,6 +78,9 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: "#636e72",
     marginBottom: 40,
+  },
+  inputGroup: {
+    marginVertical: 10,
   },
   textLabel: {
     fontFamily: "poppins-regular",
@@ -48,5 +93,16 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 12,
     paddingVertical: 5,
+  },
+  buttonStyle: {
+    width: "100%",
+    marginTop: 10,
+    textAlign: "center",
+    fontFamily: "poppins-medium",
+    letterSpacing: -0.5,
+    fontSize: 16,
+    color: "#FFF",
+    paddingVertical: 5,
+    borderRadius: 3,
   },
 });
